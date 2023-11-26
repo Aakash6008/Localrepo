47008928fdc6df5ef5b31f20a3dae4b774bf0de6
Aakash Pokhriyal <119079436+Aakash6008@users.noreply.github.com>
2023-11-26 17:18:20 +0530

    Update index.js

--- Age Calculator/index.js
+++ Age Calculator/index.js
@@ -28,30 +28,3 @@ function getAge(value){
            return year;
            }
    }
-
-//    const btnEl = document.getElementById("btn")
-//    btnEl.addEventListener("click", calculateAge)
-
-//    const result = document.getElementById("result")
-//    const DOB = document.getElementById("DOB")
-
-//    function calculateAge() {
-//        const DOBvalue = DOB.value
-//        if (DOBvalue === "") {
-//            alert("Please Enter Your Date Of Birth")
-//        } else {
-//            const age = getAge(DOBvalue)
-//            result.innerHTML = `You are ${age} years old`
-//        }
-//    }
-
-//    function getAge(DOBvalue) {
-//        const currentDate = new Date()
-//        const selectedDate = new Date(DOBvalue)
-//        const year = currentDate.getFullYear() - selectedDate.getFullYear()
-//        const month = currentDate.getMonth() - selectedDate.getMonth()
-//        if (month < 0 || (month === 0 && currentDate.getDate() < selectedDate.getDate())) {
-//            year--;
-//        }
-//        return year;
-//    }
